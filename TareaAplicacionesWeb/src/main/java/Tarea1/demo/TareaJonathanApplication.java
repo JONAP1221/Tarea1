f51b47e67431d390924728c81cdc006a2ca9cbf5
JONAP1221 <jlopez56057@ufide.ac.cr>
2024-02-09 20:01:51 -0600

    Inicio/base
    Inicio hecho y la base para el resto de paginas

---
+++ TareaAplicacionesWeb/src/main/java/Tarea1/demo/TareaJonathanApplication.java
@@ -0,0 +1,13 @@
+package Tarea1.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TareaJonathanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TareaJonathanApplication.class, args);
+	}
+
+}
